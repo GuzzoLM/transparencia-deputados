882137c76000b9cda201b13e2c4ecf41470127bc
GuzzoLM <lucasmguzzo@gmail.com>
2020-06-03 16:37:02 +0100

    Add PrimeNG top menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MenubarModule} from 'primeng/menubar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainChartComponent } from './main-chart/main-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
